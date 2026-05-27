406d7631ba71c978b45daea8c7472518839ee3f9
charan manthina <manthina.charan@gmail.com>
2026-05-27 17:02:07 +0530

    Add Java 21 installation to sonar.sh
    Install Java 21 Amazon Corretto before downloading SonarQube.

--- sonar.sh
+++ sonar.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 #Launch an instance with 9000 and t2.medium
+yum install java-21-amazon-corretto-jmods -y
 cd /opt/
 wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
 unzip sonarqube-8.9.6.50800.zip
